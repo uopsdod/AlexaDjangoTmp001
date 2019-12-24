4fa3f54e6d2108ee2db09bd8b964725c5034a7ab
sam <uopsdod@gmail.com>
2019-12-24 15:48:08 +0800

    update

--- skill/helpers/LaunchRequestHelper.py
+++ skill/helpers/LaunchRequestHelper.py
@@ -1,19 +1,16 @@
 import json
 from ..utils.common_util import UserStates
 
-class LaunchRequestHelper(object):
-    # LanchRequest
-    TAG = 'LaunchRequest'
+TAG = 'LaunchRequest'
 
-    @staticmethod
-    def execute(handler_input):
-        # type: (HandlerInput) -> Response
-        # initialize session state
-        session_attr = handler_input.attributes_manager.session_attributes
-        user_states = []
-        user_states.append(UserStates.INIT.name)
-        session_attr["user_states"] = json.dumps(user_states)
-        print(LaunchRequestHelper.TAG + ' - user_states:' + session_attr["user_states"])
-        # build response
-        speech_text = "Version one, do you want to create a new meeting system or use an existing one?"
-        return handler_input.response_builder.speak(speech_text).set_should_end_session(False).response
+def execute(handler_input):
+    # type: (HandlerInput) -> Response
+    # initialize session state
+    session_attr = handler_input.attributes_manager.session_attributes
+    user_states = []
+    user_states.append(UserStates.INIT.name)
+    session_attr["user_states"] = json.dumps(user_states)
+    print(TAG + ' - user_states:' + session_attr["user_states"])
+    # build response
+    speech_text = "Version one, do you want to create a new meeting system or use an existing one?"
+    return handler_input.response_builder.speak(speech_text).set_should_end_session(False).response
